ae7e0a8285976358db280dacf704e87cab63cb9f
Gustavo-Alvin <gustavoboygame@gmail.com>
2026-02-12 12:22:55 -0300

    consumindo API

---
+++ src/br/com/alura/screenmatch/principal/PrincipalcomBusca.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalcomBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o filme: ");
+        var busca = scanner.nextLine();
+
+        String chave = "edc831ac";
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
